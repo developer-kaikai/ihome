b7db23d291fd55a2260c6de1aa1c20005dbceb47
qiao <1170793857@qq.com>
2019-10-27 22:04:54 +0800

    测试登录

--- src/main/java/com/shixun/ihome/work/controller/OtherController.java
+++ src/main/java/com/shixun/ihome/work/controller/OtherController.java
@@ -1,16 +1,19 @@
 package com.shixun.ihome.work.controller;
 
 import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.shixun.ihome.config.ApiJsonObject;
+import com.shixun.ihome.config.ApiJsonProperty;
 import com.shixun.ihome.json.ResultBase;
 import com.shixun.ihome.work.service.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
+
+import javax.servlet.http.HttpSession;
 
 @Api(description = "杂项")
 @RestController
@@ -25,6 +28,30 @@ public class OtherController {
     public ResultBase getWeixinIdFromUserPhone(@PathVariable String phone){
         JSONArray arr = userService.getWeiXinId(phone + "%");
         return ResultBase.success(arr);
+    }
+
+    @ApiOperation(value = "登录测试")
+    @PostMapping("/pcLogin")
+    public ResultBase pcLogin (@ApiJsonObject (name = "params", value = {
+            @ApiJsonProperty(key = "name", example = "user", description = "用户名"),
+            @ApiJsonProperty(key = "password", example = "密码", description = "密码")
+    })@RequestBody JSONObject params, @ApiIgnore HttpSession session){
+        String name = params.getString("name");
+        String password = params.getString("password");
+        if (password.equals("123456")){
+            session.setAttribute("name", name);
+            return ResultBase.success();
+        }else {
+            return ResultBase.fail("密码错误");
+        }
+    }
+
+
+    @ApiOperation(value = "获取用户信息测试")
+    @GetMapping("/getUserInfo")
+    public ResultBase getUserInfo(@ApiIgnore HttpSession session){
+        String name = (String) session.getAttribute("name");
+        return ResultBase.success(name);
     }
 
 }
