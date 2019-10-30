ffe3d16d1e4b75453e07845cb3d55e9d5ec22592
qiao <1170793857@qq.com>
2019-10-30 15:12:48 +0800

    添加管理员的Service

---
+++ src/main/java/com/shixun/ihome/work/service/AdminService.java
@@ -0,0 +1,18 @@
+package com.shixun.ihome.work.service;
+
+import com.github.pagehelper.PageInfo;
+import com.shixun.ihome.publicservice.pojo.IAdministration;
+
+public interface AdminService {
+    //获取单个数据
+    IAdministration getOne(int id);
+    //添加管理员
+    boolean addAdministrationRecord(IAdministration admin, String bywho);
+    //修改管理员
+    boolean updateAdministrationRecord(IAdministration newAdmin, IAdministration oldAdmin, String bywho);
+    //根据条件查询管理员
+    PageInfo<IAdministration> getAdmins(IAdministration admin, int pageSize, int pageNum);
+    //删除管理员
+    boolean deleteAdministrationRecord(IAdministration admin, String byWho);
+
+}
